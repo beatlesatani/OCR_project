4cabbc772c5ba19f49a1e314b295c3912cbfa47f
Yusuke Satani <95377662+beatlesatani@users.noreply.github.com>
2023-11-07 00:05:05 -0500

    add codes

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import base64
 import requests
 import random
 import genanki
+import atexit
 
 app = Flask(__name__)
-# Your route for the web application
+
+# Initialize my_deck as None
+my_deck = None
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     user_input = None
@@ -19,30 +23,43 @@ def home():
             if file:
                 # Read the image file and encode it as base64
                 image_data = base64.b64encode(file.read()).decode('utf-8')
-   
-            # Retrieve the user input from the form
+
+        # Retrieve the user input from the form
         user_input = request.form.get("user_input")
         if user_input:
             search_result = search_word_in_dictionary(user_input)
-            card_gen(user_input, search_result)
+            global my_deck
+            if my_deck is None:
+                my_deck = create_deck()
+            add_card_to_deck(my_deck, user_input, search_result)
+            
     return render_template("index.html", user_input=user_input, image_data=image_data, search_result=search_result)
-
 
 # Function to search for words in the dictionary and retrieve meanings
 def search_word_in_dictionary(word):
-    response = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
+    response = requests.get(f"https://jisho.org/api/v1/search/words?keyword={word}")
     if response.status_code == 200:
         data = response.json()
-        # Parse the response and extract meanings or relevant data
-        # For example, you can access the data as data[0]["meanings"]
-        definition = data[0]["meanings"][0]["definitions"][0]["definition"]
-        return definition
+        # Assuming that `data` contains the JSON data you posted
+        meanings = []
+        for entry in data['data']:
+            senses = entry['senses']
+            for sense in senses:
+                eng_meaning = sense.get('english_definitions', [])
+                meanings.append(eng_meaning)
+        return meanings[0]
     else:
         return "Word not found or API error"
-    
-def card_gen(wor, defi):
-    fir_random = random.randrange(1 << 30, 1 << 31)
+
+# Function to create and return a my_deck
+def create_deck():
     sec_random = random.randrange(1 << 30, 1 << 31)
+    my_deck = genanki.Deck(sec_random, 'English')
+    return my_deck
+
+# Function to add a card to the my_deck
+def add_card_to_deck(my_deck, wor, defi):
+    fir_random = random.randrange(1 << 30, 1 << 31)
     my_model = genanki.Model(fir_random, 'English',
                             fields=[
                                 {'name': 'Question'},
@@ -55,32 +72,15 @@ def card_gen(wor, defi):
                                     'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
                                 },
                             ])
-    my_note = genanki.Note(model=my_model, fields=[wor, defi])
-    my_deck = genanki.Deck(sec_random, 'English')
+    my_note = genanki.Note(model=my_model, fields=[str(wor), str(defi)])
     my_deck.add_note(my_note)
-    genanki.Package(my_deck).write_to_file('output.apkg')
-   
 
+# Function to write the my_deck to the output.apkg file
+def write_deck_to_file():
+    if my_deck:
+        genanki.Package(my_deck).write_to_file('output.apkg')
 
-   
-
-'''   
-# Function to create a Quizlet set and add terms and definitions
-def make_cards(accessToken, setName, terms, definitions):
-    # Use Quizlet API to create a set
-    # Iterate through terms and definitions and add them to the set
-'''
-
+atexit.register(write_deck_to_file)
 
 if __name__ == '__main__':
     app.run()
-
-"""
-So far, codes works,
-1.upload the picture from local 
-2.display the picture on the same screen
-
-from now, i'll work on the image processing using trained_model.
-so, I'll back to here to integrate them
-
-"""
